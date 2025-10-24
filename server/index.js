afc98d023911b60b3c699eda9de0e43213fc554b
dabin2011 <dabin201100@gmail.com>
2025-10-24 20:48:44 +0900

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('接続:', socket.id);
+
+  socket.on('chat', (msg) => {
+    io.emit('chat', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('切断:', socket.id);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('ライブコメントサーバー稼働中');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`サーバー起動中: http://localhost:${PORT}`);
+});
